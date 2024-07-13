b97513d2d267b6c57e042d676116301676985598
esoty6 <kamilwitek12@o2.pl>
2024-07-13 12:12:06 +0200

    Header component

---
+++ app/src/header/header.component.spec.ts
@@ -0,0 +1,38 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserService } from '../services/user.service';
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeaderComponent],
+      providers: [UserService],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render text', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.textContent).toContain('Zadanie rekrutacyjne');
+  });
+
+  it('should render user name', () => {
+    const service = TestBed.inject(UserService);
+    const compiled = fixture.nativeElement as HTMLElement;
+    service.setUserName('Test Name');
+    service.toggleShowUserName();
+    fixture.detectChanges();
+    expect(compiled.textContent).toContain('Test Name');
+  });
+});
